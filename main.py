554fcf760068de67bd3cf64b8e7d03b26878efe2
keepnad <8678DPgh8678>
2018-12-03 17:25:08 -0600

    more code

--- main.py
+++ main.py
@@ -1,10 +1,19 @@
-from PyDictionary import PyDictionary
+import numpy as np
+import nltk
+from nltk.corpus import wordnet as wn
+import gensim
+from gensim.models import Word2Vec
 import random
 import neural_net as nn
 
-dictionary = PyDictionary();
+print('Loading Word2Vec model, please wait...')
+model = gensim.models.KeyedVectors.load_word2vec_format('trained-model.bin', binary=True)
+print('Word2Vec model loaded.')
 
-definition = dictionary.meaning("animal");
 
-for part_of_speech in definition:
-    print(part_of_speech);
+tokens = nltk.word_tokenize('')
+vectors = [model[w] for w in tokens]
+
+for i in range(len(tokens)):
+    print('\n', tokens[i])
+    print (vectors[i])
--- neural_net.py
+++ neural_net.py
@@ -16,19 +16,73 @@ def derivative_sigmoid(x):
 
 
 class NeuralNetwork:
-    def __init__(self, number_inputs, number_hidden, number_outputs, eta):
-        self.inputs = number_inputs
-        self.hidden_nodes = number_hidden
-        self.classes = number_outputs
+    def __init__(self, eta):
+        self.inputs = 300
+        self.hidden_nodes = 300
+        self.outputs = 5
 
         self.eta = eta
         self.confidence = 0
 
         self.bias_bottom = [rand() for i in range(self.hidden_nodes)]
-        self.bias_top = [rand() for i in range(self.classes)]
+        self.bias_top = [rand() for i in range(self.outputs)]
         self.weight_bottom = [[rand() for i in range(self.hidden_nodes)] for j in range(self.inputs)]
-        self.weight_top = [[rand() for i in range(self.classes)] for j in range(self.hidden_nodes)]
+        self.weight_top = [[rand() for i in range(self.outputs)] for j in range(self.hidden_nodes)]
 
         self.hidden = [0.0] * self.hidden_nodes
-        self.output = [0.0] * self.classes
+        self.output = [0.0] * self.outputs
 
+    def predict(self, sample):
+        i = 0
+        for k in range(self.hidden_nodes):
+            weight_sum = 0.0
+            for i in range(self.inputs):
+                weight_sum += self.weight_bottom[i][k] * sample[i]
+            weight_sum += self.bias_bottom[k]
+            self.hidden[k] = sigmoid(weight_sum)
+
+        for k in range(self.outputs):
+            weight_sum = 0.0
+            for i in range(self.hidden_nodes):
+                weight_sum += self.weight_top[i][k] * self.hidden[i]
+            weight_sum += self.bias_top[k]
+            self.output[k] = sigmoid(weight_sum)
+
+        for k in range(self.outputs):
+            if k == 0 or self.output[k] > self.output[i]:
+                i = k
+
+        next_max_sum = -1.0
+        for k in range(self.outputs):
+            if k != i:
+                if next_max_sum < 0.0 or self.output[k] > next_max_sum:
+                    next_max_sum = self.output[k]
+        self.confidence = self.output[i] - next_max_sum
+
+        return i
+
+    def adjust_weights(self, sample, actual):
+        delta = [0] * self.outputs
+        for k in range(self.outputs):
+            if k == actual:
+                weight_sum = 1.0
+            else:
+                weight_sum = 0.0
+            weight_sum -= self.output[k]
+
+            delta[k] = weight_sum * derivative_sigmoid(self.output[k])
+
+            for i in range(self.hidden_nodes):
+                self.weight_top[i][k] += self.eta * delta[k] * self.hidden[i]
+
+            self.bias_top[k] += self.eta * delta[k]
+
+        for j in range(self.hidden_nodes):
+            d = 0.0
+            for k in range(self.outputs):
+                d += self.weight_top[j][k] * delta[k]
+
+            for i in range(self.inputs):
+                self.weight_bottom[i][j] += self.eta * derivative_sigmoid(self.hidden[j]) * d * sample[i]
+
+            self.bias_bottom[j] += self.eta * d
