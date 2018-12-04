8377389cac8ee5415f0db2bc5b2b02e3d6b92d7b
Mcanas17 <michaelcanas08@yahoo.com>
2018-12-03 19:44:56 -0600

    update tag

--- textTag.py
+++ textTag.py
@@ -1,7 +1,10 @@
 from nltk.corpus import wordnet as wn
 import collections
 
-articles = ['a','an','the']
+articles = ['a','an','the','these','this']
+
+with open("adverbs","r") as file0:
+   adv = file0.read().splitlines()
 
 with open("adj","r") as file1:
    adj = file1.read().splitlines()
@@ -22,28 +25,31 @@ for line in open("sentences",'r'):
    words[0] = words[0][0].lower() + words[0][1:] #lower case first word
    words[-1] = words[-1][:-1]
 
-   doFlag = 0
-   ioFlag = 0
+   morphFlag = 0
    i = 0
    while i < len(words):
+	#print words[i]
+	#print wordD
    	if words[i] in articles:
 	   tmp = (words[i],"article")
 	   wordD.append(tmp)
 	elif words[i] in preps:
 	   tmp = (words[i],"preposition")
 	   wordD.append(tmp)
-	elif i+1 < len(words) and words[i] in nouns and words[i+1] in nouns:
-		tmp = (words[i],"adjective")
-	   	wordD.append(tmp)
-	   	if doFlag == 0:
-	   	   tmp = (words[i+1],"directObj")
-	     	   wordD.append(tmp)
-		   doFlag = 1
-	   	else:
-	  	   tmp = (words[i+1],"indirectObj")
-		   wordD.append(tmp)
-		   ioFlag = 1
-	   	i += 1
+
+ 	elif i+1 < len(words) and words[i] in adj and words[i+1] in nouns:
+           tmp = (words[i],"adjective")
+           wordD.append(tmp)
+           tmp = (words[i+1],"noun")
+           wordD.append(tmp)
+           i += 1
+
+	elif i+1 < len(words) and words[i] in adv and words[i+1] in adj and words[i+1] not in preps:
+	   tmp = (words[i],"adverb")
+	   wordD.append(tmp)
+	   tmp = (words[i+1],"verb")
+	   wordD.append(tmp)
+	   i += 1
 
 	elif words[i] in verbs:
 	   tmp = (words[i],"verb")
@@ -52,18 +58,21 @@ for line in open("sentences",'r'):
 	   tmp = words[i],"adjective"
 	   wordD.append(tmp)
 	elif words[i] in nouns:
-	   if doFlag == 0:
-		tmp = (words[i],"directObj")
-	   else:
-		tmp = (words[i],"indirectObj")
+	   tmp = (words[i],"noun")
 	   wordD.append(tmp)
+	#elif "'" in words[i]:
+	#   tmp = (words[i],"conjunction")
+	#   wordD.append(tmp)
 	else:
-	   words[i] = wn.morphy(words[i])
-	   if words[i] in verbs:
-		i -= 1
+	   if morphFlag == 0:
+	   	words[i] = wn.morphy(words[i])
+	   	i -= 1
+	   	morphFlag = 1
 	   else:
-		i -= 2
+		tmp = (words[i],"unkown")
+		wordD.append(tmp)
+		
 	i+=1
 
-   print wordD
+   print wordD,"\n"
 
